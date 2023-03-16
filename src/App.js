404d408b7995ca5d42153dbe5fb511b2064d5d0d
Gitroars <yuwonoarvin@gmail.com>
2023-03-16 23:18:33 +0700

    Revert "Revert "WIP Authentication""
    This reverts commit 56a747f2ad14e3b19d3a25440de19815803e426d.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,72 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
-import { BrowserRouter as Router,Route } from "react-router-dom";
+import { createUserWithEmailAndPassword,signInWithEmailAndPassword,
+onAuthStateChanged,signOut } from "firebase/auth";
 import Title from "./components/Title";
-import Register from "./components/Register";
+import {auth} from "./firebase"
 
 function App() {
+  const [registerEmail,setRegisterEmail] = useState("")
+  const [registerPassword,setRegisterPassword] = useState("")
+  const [loginEmail,setLoginEmail] = useState("")
+  const [loginPassword,setLoginPassword] = useState("")
+  const [user,setUser] = useState({});
+
+  onAuthStateChanged(auth,(currentUser)=>{
+    setUser(currentUser);
+  });
+
+  const register = async () =>{
+    try{
+      const user = await createUserWithEmailAndPassword(
+        auth,registerEmail,registerPassword
+      );
+      console.log(user);
+    } catch(error){
+      console.log(error.message);
+    }
+  };
+
+  const login = async () => {
+    try {
+      const user = await signInWithEmailAndPassword(
+        auth,
+        loginEmail,
+        loginPassword
+      );
+      console.log(user);
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  const logout = async () => {
+    await signOut(auth);
+  };
+
+
+
   return (
     <div className="app">
-      
+      {/*Registration Form */}
+      <div>
+        <h3>User Registration</h3>
+        <input placeholder="Email" onChange = {(e)=>{setRegisterEmail(e.target.value);
+        }}></input>
+        <input placeholder="Password" onChange={(e)=>{setRegisterPassword(e.target.value);
+        }}></input>
+        <button onClick={register}>Register</button>
+      </div>
+        {/*Login Form */}
+      <div>
+        <h3>User Login</h3>
+        <input placeholder="Email" onChange={(e)=>{setLoginEmail(e.target.value);}}></input>
+        <input placeholder="Password" onChange={(e)=>{setLoginPassword(e.target.value);}}></input>
+        <button onClick={login}>Login</button>
+      </div>
+      <h4>User: </h4>
+      {user?.email}
+      <button onClick={logout}>Logout</button>
       <Title />
       
     </div>
